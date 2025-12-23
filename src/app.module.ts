1db663f4f86712de9a3ce45334d49da8bc1282ff
BKTS-NL206\Brackest <ayyaz.boota@bracketsltd.email>
2025-12-23 17:35:13 +0500

    Revert "Add config module"
    This reverts commit b5d6c649631e51fbd80c5ce71aca138cd2329164.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigsModule } from './configs/configs.module';
 
 @Module({
-  imports: [ConfigsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
